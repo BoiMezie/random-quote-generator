b90224af8d12c01b5aabeefac55618cac19e3b85
BoiMezie <chiemezieobialo@gmail.com>
2025-12-24 21:27:42 +0100

    Finally working

--- js.js
+++ js.js
@@ -1,37 +1,26 @@
 // const btn=document.getElementsByClassName(".btn");
 
-/* const apiUrl = "https://api.quotable.io/quotes/random";
+const apiUrl = "https://api.quotable.io/quotes/random";
 
 async function fetchRandomQuote(url) {
-  const response = await fetch(url);
-  var data = await response.json();
-  console.log(data);
-  // document.querySelector(".quote").textContent = 'Holla';
-  document.getElementById(".author").textContent = data.author;
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    //API returns an array
+    const quote = data[0];
+
+    document.querySelector(".quote").textContent = data[0].content;
+    document.querySelector(".author").textContent = data[0].author;
+    // console.log(data);
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
 }
 
-fetchRandomQuote(apiUrl);
- */
 document
   .querySelector(".generate-quote")
-  .addEventListener("click", function () {
-    document.querySelector(".quote").textContent = "Holla";
-  });
-//   try {
-//     const response = await fetch(apiUrl);
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     var data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.error("There has been a problem with your fetch operation:", error);
-//   }
-// }
+  .addEventListener("click", fetchRandomQuote);
 
-// fetchRandomQuote(apiUrl);
-// fetchRandomQuote().then((quote) => {
-//   if (quote) {
-//     console.log(`"${quote.content}" - ${quote.author}`);
-//   }
-// });
+fetchRandomQuote(apiUrl);
+
