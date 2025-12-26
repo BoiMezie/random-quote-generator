59a238845fa278b3130d7a62ff9a497bc9625b88
BoiMezie <chiemezieobialo@gmail.com>
2025-12-26 14:21:51 +0100

    Editing theshare button

--- js.js
+++ js.js
@@ -24,3 +24,35 @@ document
 
 fetchRandomQuote(apiUrl);
 
+const modal = document.querySelector(".modal");
+// const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector(".close-modal");
+const buttonShowModal = document.querySelector(".show-modal");
+// console.log(buttonShowModal);
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  // overlay.classList.remove('hidden');
+};
+
+buttonShowModal.addEventListener("click", openModal);
+
+/* for (let i = 0; i < buttonShowModal.length; i++) {
+} */
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  // overlay.classList.add('hidden');
+};
+
+buttonCloseModal.addEventListener("click", closeModal);
+
+// overlay.addEventListener('click', closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape") {
+    return closeModal();
+  }
+});
